c64a42fd6e0a13e27c03327dfff79cb6a4cca601
nguyenvy <nguyenvyqn21@gmail.com>
2018-12-10 15:28:23 +0700

    chapter11

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save{email.downcase!}
+
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true, length: {maximum: Settings.name_max_length}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: Settings.email_max_length},
@@ -39,5 +41,16 @@ class User < ApplicationRecord
   def log_out
     session.delete :user_id
     @current_user = nil
+  end
+  
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
---
+++ db/migrate/20181210065548_add_activation_to_users.rb
@@ -0,0 +1,4 @@
+class AddActivationToUsers < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
